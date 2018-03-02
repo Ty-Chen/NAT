066cdfc4928ac2dc57760db5b8853902e199c0d8
Ty-Chen <369494003@qq.com>
2018-03-02 23:33:59 +0800

    Update list.h

--- littleTry/list.h
+++ littleTry/list.h
@@ -49,13 +49,25 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
 }
 
 /**
- * list_for_each_entry	-	iterate over list of given type
+ * list_for_each	-	iterate over list 
  * @pos:	the type * to use as a loop cursor.
  * @head:	the head for your list.
  * @member:	the name of the list_head within the struct.
  */
-#define list_for_each_entry(pos, head, member)				\
-	for (pos = list_entry((head)->next, typeof(*pos), member);	\
-	     &pos->member != (head); 	\
-	     pos = list_entry(pos->member.next, typeof(*pos), member))
+#define list_for_each(pos, head) for (pos = (head)->next; pos != (head); pos = pos->next)
 
+/**
+ * list_for_each_r	-	iterate over list 
+ *
+ */
+#define list_for_each_r(pos, head) for (pos = (head)->prev; pos != (head); pos = pos->prev)
+
+/**
+ * insert a new entry between two known consecutive entries.
+ *
+ * This is only for internal list manipulation where we konw
+ * the prev/next entries already
+ */
+static inline void __list_add(struct list_head *new,
+			     struct list_head *prev,
+			     struct list_head *next)
