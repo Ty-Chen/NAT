0a1cec2256deabb655cbe19994214c6841ef7570
Ty-Chen <369494003@qq.com>
2018-03-03 23:48:30 +0800

    Update list.h

--- littleTry/list.h
+++ littleTry/list.h
@@ -71,3 +71,5 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
 static inline void __list_add(struct list_head *new,
 			     struct list_head *prev,
 			     struct list_head *next)
+
+	
