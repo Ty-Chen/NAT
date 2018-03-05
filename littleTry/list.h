5e4610b03122fc45696ce7c6c39acb74f5505a80
Ty-Chen <369494003@qq.com>
2018-03-05 22:38:04 +0800

    Update list.h

--- littleTry/list.h
+++ littleTry/list.h
@@ -71,6 +71,35 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
 static inline void __list_add(struct list_head *new,
 			     struct list_head *prev,
 			     struct list_head *next)
+{
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
+}
 
-	
-	
+/**
+ * list_add    -     add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it after
+ *
+ * Insert a new entry after the specified head.
+ * This is good for implenting stacks.
+ */	
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head, head->next);
+}
+
+/**
+ * list_add_tail    -     add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it before
+ *
+ * Insert a new entry before the specified head.
+ * This is good for implenting queue.
+ */	
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head->prev, head);
+}
