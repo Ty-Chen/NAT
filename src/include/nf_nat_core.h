addf636ac35176b57ac6a4cbbc81305be732f58a
Ty-Chen <369494003@qq.com>
2018-03-08 23:29:18 +0800

    Create nf_nat_core.h

---
+++ src/include/nf_nat_core.h
@@ -0,0 +1,20 @@
+#ifndef _NF_NAT_CORE_H
+$define _NF_NAT_CORE_H
+#include <linux/list.h>
+#include <net/netfilter/nf_conntrack.h>
+#include <net/netfilter/nf_nat.h>
+#include <net/netfilter/nf_conntrack_tuple.h>
+
+unsigned int nf_nat_packet(struct nf_conn *ct, enum ip_conntrack_info ctifo,
+                            unsigned int hooknum, struct sk_buff *skb);
+                            
+int nf_xfrm_me_harder(struct net *net, struct sk_buff *skb, unsigned int family);
+
+struct inline int nf_nat_initialized(struct nf_conn *ct, 
+                                    enum nf_nat_manip_type manip)
+{
+    if (manip == NF_NAT_MANIP_SRC)
+        return ct->status & IPS_SRC_NAT_DONE;
+    else 
+        return ct->status & IPS_DST_NAT_DONE;
+}
