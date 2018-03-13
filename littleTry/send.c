9f58128c1baf524e609f9fb8fe892391d74e6501
Ty-Chen <369494003@qq.com>
2018-03-13 23:05:25 +0800

    Update send.c

--- littleTry/send.c
+++ littleTry/send.c
@@ -1,4 +1,19 @@
 #include <linuc/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include
+#include <linux/netdevice.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/types.h>
+#include <linux/skbuff.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <net/tcp.h>
+#include <net/udp.h>
+#include <net/icmp.h>
+#include <linux/inet.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+#define ETH "eth0"
